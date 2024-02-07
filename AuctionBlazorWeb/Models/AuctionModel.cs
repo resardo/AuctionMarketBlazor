fe71d830e0d93b5bbfcf12f9b9dbf406a812709f
Resardo <62570251+Resardo@users.noreply.github.com>
2024-02-07 16:57:01 +0100

    small changes

--- AuctionBlazorWeb/Models/AuctionModel.cs
+++ AuctionBlazorWeb/Models/AuctionModel.cs
@@ -12,8 +12,13 @@ namespace AuctionBlazorWeb.Models
 
         [Required(ErrorMessage = "Description is required."), MinLength(10), MaxLength(100)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "StartTime is required.")]
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+
+        [Required(ErrorMessage = "StartPrice is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "StartPrice must be greater than or equal to 0.")]
         public decimal StartPrice { get; set; }
         
     }
